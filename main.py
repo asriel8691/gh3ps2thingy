462ec399ebbaf88578b80418a885349b2a0f9338
Asriel <asriel.oficial@gmail.com>
2025-03-20 15:57:16 -0300

    v1.5 - labels
    all labels with better names, little rework on listchannels and createmsv

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-def preparacao(option, extensions): # lista os arquivos a serem processados com base nas extensões
+def listfiles(option, extensions): # lista os arquivos a serem processados com base nas extensões
 
     currentExecutedDir = os.getcwd()  # pega o diretorio atual
     fileList = [] # guarda os arquivos listados
@@ -10,9 +10,9 @@ def preparacao(option, extensions): # lista os arquivos a serem processados com 
     for file in os.listdir(currentExecutedDir): # para cada arquivo no diretorio atual
         fileLowercaseName = file.lower() # o nome do arquivo ficará em minusculo para usar a comparação do 'extensions'
 
-        for fileExtensions in extensions: # para cada extensão no parametro extensions
+        for fileExtension in extensions: # para cada extensão no parametro extensions
 
-            if fileExtensions in fileLowercaseName: # se o nome tiver a extensao
+            if fileExtension in fileLowercaseName: # se o nome tiver a extensao
                 fileWasFound = True # marca o encontrou arquivos como true
 
                 if file not in fileList: # se o arquivo não estiver ainda na lista
@@ -22,49 +22,50 @@ def preparacao(option, extensions): # lista os arquivos a serem processados com 
         print('\nNo files to work with\nCheck for typos in file names')
     else:
         if option == 1: # se a opção for X
-            listar_canais(fileList) # ativa a função levando a lista de arquivos junto
+            listchannels(fileList) # ativa a função levando a lista de arquivos junto
         elif option == 2 or option == 3:
             vagparameter(fileList)
         elif option == 4:
-            inverte_colunas(fileList)
+            swapcolumns(fileList)
 
 
-def inverte_colunas(lista_arquivos):
+def swapcolumns(file_list):
 
-    for imagem in lista_arquivos: # para cada imagem na lista de arquivos
+    for image in file_list: # para cada imagem na lista de arquivos
 
-        if "_inv" in imagem: # se tiver _inv no nome
-            print("\nSkipping {} (already swapped)".format(imagem))
+        if "_inv" in image: # se tiver _inv no nome
+            print("\nSkipping {} (already swapped)".format(image))
             continue
 
-        nome, ext = os.path.splitext(imagem) # separa o nome da extensão
-        img = Image.open(imagem) # abre a imagem como img
-        pixels = img.load()
-        largura, altura = img.size # pega a largura e a altura da img
-        print("\nSwapping {}".format(imagem))
+        imageName, imageExt = os.path.splitext(image) # separa o nome da extensão
+        inImage = Image.open(image) # abre a imagem como img
+        inImagePixels = inImage.load()
+        inImageWidth, inImageHeight = inImage.size # pega a largura e a altura da img
 
-        saida = img.copy() # copia a imagem para a saida
-        saida_pixels = saida.load() # carrega cada pixel no saida_pixels
-        saida_nome = nome + "_inv" + ext # o nome e a extensão será o mesmo junto com _inv, mostrando que inverteu as colunas
+        outImage = inImage.copy() # copia a imagem para a saida
+        outImagePixels = inImage.load() # carrega cada pixel no saida_pixels
+        outImageName = imageName + "_inv" + imageExt # o nome e a extensão será o mesmo junto com _inv, mostrando que inverteu as colunas
 
-        for x in range(0, largura - 1, 2): # largura - 1 garante que as imagens impares não troquem a ultima coluna
-            for y in range(altura): # repete a inversão em toda a altura
-                saida_pixels[x, y] = pixels[x + 1, y]  # coluna impar da saída será a par da entrada
-                saida_pixels[x + 1, y] = pixels[x, y]  # coluna par da saída será a ímpar da entrada
+        print("\nSwapping {}".format(image))
 
-        saida.save(saida_nome, icc_profile = img.info.get("icc_profile")) #iccprofile será o mesmo do input. se tiver o icc do photoshop, manterá saturado no final
+        for x in range(0, inImageWidth - 1, 2): # largura - 1 garante que as imagens impares não troquem a ultima coluna
+            for y in range(inImageHeight): # repete a inversão em toda a altura
+                outImagePixels[x, y] = inImagePixels[x + 1, y]  # coluna impar da saída será a par da entrada
+                outImagePixels[x + 1, y] = inImagePixels[x, y]  # coluna par da saída será a ímpar da entrada
+
+        outImage.save(outImageName, icc_profile = inImage.info.get("icc_profile")) #iccprofile será o mesmo do input. se tiver o icc do photoshop, manterá saturado no final
         print("Column swapped successfully!")
 
-def vagparameter(lista_arquivos):
+def vagparameter(file_list):
 
     sfxWasFoundCounter = 1 # conta os arquivos passados
 
-    for file in lista_arquivos: # para cada arquivo na lista_arquivos
+    for file in file_list: # para cada arquivo na lista_arquivos
 
         if option == 3 and file == 'sfx.wad': # se a opção for 3 e for sfx.wad
             sfxExtractPath = str(input('\nExtract sfx.wad to (ex: C:/sfxrip): ')) # pede o diretório para extração
 
-        elif sfxWasFoundCounter == len(lista_arquivos) and option == 3: # se não achou
+        elif sfxWasFoundCounter == len(file_list) and option == 3: # se não achou
             print('\nNo sfx.wad to extract')
             return # sai da função sem retorno
 
@@ -130,141 +131,154 @@ def sfxripper(block_name, block_bytes, extract_path): # quando a opção for 3 e
     with open(outFileExtractPath, 'wb') as outFile: # abre o caminho de saida como arquivo para escrita em bytes
         outFile.write(block_bytes) # escreve/extrai o bloco nele
     
-    print("{} extracted".format(outFileName)) # printa
+    print("{} extracted".format(outFileName))
 
 
 
-def listar_canais(lista_arquivos): # lista todos os arquivos vag e prepara em casais para ser processados
+def listchannels(file_list): # lista todos os arquivos vag e prepara em casais para ser processados
 
-    casais = []
-    encontrou_arquivos = False
+    vagPairs = []
+    vagWasFound = False
 
-    for arquivo in lista_arquivos:
+    for file in file_list:
 
-        if arquivo.endswith("L.vag") or arquivo.endswith("R.vag"): # se terminar em l ou r
-            encontrou_arquivos = True # encontrou arquivo
-            casal = arquivo[:-5]  # remove até o l/r, sendo o casal atual
+        if file.endswith("L.vag") or file.endswith("R.vag"): # se terminar em l ou r
+            vagWasFound = True # encontrou arquivo
+            pairName = file[:-5]  # remove até o l/r, sendo o par atual
 
-            if casal not in casais: # se o casal não estiver na lista
-                casais.append(casal) # coloca nela
+            if pairName not in vagPairs: # se o par não estiver na lista
+                vagPairs.append(pairName) # coloca nela
     
-    if encontrou_arquivos == False: # se não tiver arquivos na pasta
+    if vagWasFound == False: # se não tiver arquivos na pasta
         print('\nNo files to convert\nCheck for typos in file names')
     
-    for casal in casais: # para cada casal na lista de casais
+    for pairName in vagPairs: # para cada par na lista de casais
 
-        arquivoL = casal + "L.vag"
-        arquivoR = casal + "R.vag"
+        vagChannelL = pairName + "L.vag"
+        vagChannelR = pairName + "R.vag"
 
-        if arquivoL in lista_arquivos and arquivoR in lista_arquivos: # se tiver ambos os canais
-            print("\nChecking pair {}".format(casal))
+        print("\nChecking pair {}".format(pairName))
 
-            arquivosMSV = [] # lista para os msv criados para então ser usado no mesclar arquivos
-            arquivosVAG = [arquivoL, arquivoR] # lista para criar o msv do casal, assim não alterando os arquivos vag originais
-
-            for arquivoVAG in arquivosVAG: # para cada vag
-
-                arquivoMSV = arquivoVAG[:-4] + ".msvs" # atualiza o nome do arquivo msv a cada iteração (L dps R)
-                print("Creating {}".format(arquivoMSV))
-
-                with open(arquivoVAG, 'rb') as arquivo_origem: # abre o arquivo original para copiar todo o conteudo
-                    conteudo = arquivo_origem.read() # o conteudo terá todos os bytes do arquivo_origem / vag
-
-                with open(arquivoMSV, 'wb') as arquivo_preparado: # cria/abre o arquivo preparado para colar o conteudo do original
-                    arquivo_preparado.write(conteudo) # o conteudo é passado para o arquivo_preparado / msv
-
-                arquivosMSV.append(arquivoMSV) # após a clonagem, o msv criado é listado no arquivos msv
-
-            mesclar_arquivos(arquivosMSV, 0x20000, casal)
-
+        if vagChannelL in file_list and vagChannelR in file_list: # se tiver ambos os canais
+            
+            msvsPair = [createmsvs(vagChannelL), createmsvs(vagChannelR)] # após a clonagem, o msv criado é listado no arquivos msv
+            createmsv(msvsPair, 0x20000, pairName)
 
         else:
-            if arquivoL not in lista_arquivos: # se não tiver um dos canais do casal atual no arquivos
-                falta = "L"
+            if vagChannelL not in file_list: # se não tiver um dos canais do par atual no arquivos
+                vagPairMissing = "L"
             else:
-                falta = "R"
-            print("\nIncomplete pair: {} (Missing {} channel)\nCheck for typos in file name or missing file".format(casal, falta))
+                vagPairMissing = "R"
+            print("\nIncomplete pair: {} (Missing {} channel)\nCheck for typos in file name or missing file".format(pairName, vagPairMissing))
 
 
-def alterar_cabecalho(filepath): # altera o cabeçalho para ter MSV e o nome do arquivo
 
-    print('Changing header {}'.format(filepath))
+def createmsvs(vag_channel): # cria os canais msvs a partir dos vags
 
-    with open(filepath, "r+b") as arquivo: # abre como ler e escrever em binário
-        cabecalho = arquivo.read(48)  # lê os primeiros 48 bytes (0x00 até 0x2F)
+    msvsChannel = vag_channel[:-4] + ".msvs" # coloca o nome e o canal do vag ao msvs
+    print("Creating {}".format(msvsChannel))
+
+    with open(vag_channel, 'rb') as vagFile: # abre o vag channel atual do par
+        vagContent = vagFile.read() # guarda todo o conteudo do vag na variavel
+
+    with open(msvsChannel, 'wb') as msvsFile: # cria/abre o arquivo msvs
+        msvsFile.write(vagContent) # escreve todo o conteudo do vag no msvs
+
+    return msvsChannel # retorna o nome canal do msvs
+
+
+
+def headerchanger(msvs_channel): # altera o cabeçalho do canal atual msvs
+
+    print('Changing header {}'.format(msvs_channel))
+
+    with open(msvs_channel, "r+b") as msvsFile: # abre como ler e escrever em bytes
+        msvsHeader = msvsFile.read(3)  # lê os primeiros 48 bytes (0x00 até 0x2F)
         
-        if cabecalho[:3] == b'VAG':  # verifica se os primeiros 3 bytes são 'VAG'
-            arquivo.seek(0)  # move o ponteiro para o início
-            arquivo.write(b'MSV' + cabecalho[3:])  # substitui 'VAG' por 'MSV'
+        if msvsHeader == b'VAG':  # verifica se os primeiros 3 bytes são 'VAG'
+            msvsFile.seek(0)  # move o ponteiro para o início
+            msvsFile.write(b'MSV')  # substitui 'VAG' por 'MSV'
 
-            arquivo.seek(32) # o ponteiro fica no 0x20, no início do nome do arquivo
+        msvsFile.seek(32) # move o ponteiro ao 0x20 a partir do inicio, parando na linha do nome interno
 
-            nome = (filepath.split('.')[0])[:-1] # (arquivo) L.msv (divide o nome do arquivo em 2 pelo '.', seleciona o primeiro item e depois corta o lado (L/R))
-            lado = (filepath.split('.')[0])[-1] # arquivo (L) .msv
+        msvsName = (msvs_channel.split('.')[0])[:-1] # (arquivo) L.msv (divide o nome do arquivo em 2 pelo '.', seleciona o primeiro item e depois corta o lado (L/R))
+        msvsChannelSide = (msvs_channel.split('.')[0])[-1] # arquivo (L) .msv
 
-            arquivo.write((nome[:15] + lado).encode('utf-8')) # o nome é limitado a 15 chars. o lado ficará no final do nome mesmo se passar do limite
-            tirar_trigger_final(filepath)
-        
+        msvsFile.write((msvsName[:15] + msvsChannelSide).encode('utf-8')) # o nome é limitado a 15 chars. o lado ficará no final do nome mesmo se passar do limite
 
-def tirar_trigger_final(filepath): # tira o trigger de parar a música no final do arquivo
-
-    print('Cutting end flag {}'.format(filepath))
-
-    with open(filepath, "r+b") as arquivo: # abre como ler e escrever em binário
-
-        arquivo.seek(-31, 2) # move o ponteiro para o final do arquivo e 31 para trás
-        rodape = arquivo.read(31) # lê o final do arquivo
-
-        if rodape[:1] == b'\x01': # se o primeiro byte for 0x01
-            rodape = b'\x00' * 31 # o rodapé será reescrito com 0x00
-            arquivo.seek(-31, 2) # move o ponteiro novamente
-            arquivo.write(rodape) # o rodapé é escrito no arquivo atual
+        endtriggerremove(msvs_channel) # chama para remover o trigger final do canal atual
 
 
-def ajustar_tamanho_para_multiplo(filepath, tamanho_multiplo):
-    # Abrir o arquivo no modo binário para leitura
-    with open(filepath, "rb") as arquivo:
-        conteudo = arquivo.read()  # Lê todo o conteúdo do arquivo como bytes
 
-    tamanho_atual = len(conteudo)  # Obtém o tamanho atual do arquivo
-    restante = tamanho_multiplo - (tamanho_atual % tamanho_multiplo)  # Calcula os bytes faltantes para o próximo múltiplo
+def endtriggerremove(msvs_channel): # tira o trigger de parar a música no final do canal
 
-    if restante != tamanho_multiplo:  # Se o arquivo não for múltiplo exato do tamanho desejado
-        conteudo += b'\x00' * restante  # Adiciona bytes nulos (0x00) para completar o tamanho
+    print('Cutting end flag {}'.format(msvs_channel))
 
-    return conteudo  # Retorna o conteúdo ajustado para uso posterior
+    with open(msvs_channel, "r+b") as msvsFile: # abre como ler e escrever em bytes
+
+        msvsFile.seek(-31, 2) # move o ponteiro para o final e 0x1F para trás
+        msvsFooter = msvsFile.read(31) # lê o final do arquivo
+
+        if msvsFooter[:1] == b'\x01': # se o primeiro byte for 0x01
+            # msvsFooter = b'\x00' * 31 # o rodapé será reescrito com 0x00
+            msvsFile.seek(-31, 2) # move o ponteiro novamente
+            msvsFile.write(b'\x00' * 31) # o rodapé é escrito no arquivo atual
 
 
-def mesclar_arquivos(lista_arquivos, tamanho_multiplo, arquivo_saida):
-    """
-    Combina os arquivos fornecidos em blocos de `tamanho_multiplo` (0x20000).
-    Alterna blocos de cada arquivo até que todos sejam processados.
-    """
-    arquivos_processados = [] # cria uma lista limpa para os arquivos processados em bytes
-    for arquivo in lista_arquivos: # para cada arquivo da lista_arquivo (casal de L e R)
-        print('\nStarting process {}'.format(arquivo))
-        alterar_cabecalho(arquivo)
-        arquivos_processados.append(ajustar_tamanho_para_multiplo(arquivo, tamanho_multiplo)) # chama a função para ajustar o tamanho do lado atual e coloca ela na lista
-        print('Adjusted size {}'.format(arquivo))
 
-    
-    # Determina o número máximo de blocos em qualquer arquivo
-    num_blocos = max(len(arquivo) // tamanho_multiplo for arquivo in arquivos_processados)
+def adjustsize(msvs_channel, block_byte_size):
+
+    with open(msvs_channel, "rb") as msvsFile: # abre o canal msvs para ler em bytes
+        msvsContent = msvsFile.read()  # guarda todos os bytes dele
+
+    msvsSize = len(msvsContent)  # pega o tamanho do conteudo
+    msvsSizeRemain = block_byte_size - (msvsSize % block_byte_size)  # calcula os bytes que faltam para o próximo multiplo
+
+    if msvsSizeRemain != block_byte_size:  # se o tamanho restante for diferente do tamanho do bloco
+        msvsContent += b'\x00' * msvsSizeRemain  # preenche o conteudo com bytes vazios usando a quantidade do restante
+
+    return msvsContent  # retorna o CONTEÚDO processado, não o nome do canal
+
+
+
+def createmsv(msvs_pair, block_byte_size, pair_name):
+
+    msvsPairContent = [] # cria uma lista limpa para os arquivos msvs em bytes
+
+    for msvsChannel in msvs_pair: # para cada canal do par msvs
+
+        print('\nStarting process {}'.format(msvsChannel))
+
+        headerchanger(msvsChannel) # muda o cabeçalho (e o rodapé)
+        msvsPairContent.append(adjustsize(msvsChannel, block_byte_size)) # chama a função para ajustar o tamanho do canal atual e coloca o CONTEÚDO na lista
+
+        print('Adjusted size {}'.format(msvsChannel))
+
+    msvsPairBlocks = [] # lista vazia para guardar a quantidade de blocos do tamanho block_byte_size cabem no conteudo
+
+    # esse for abaixo acho que não é necessário, já que os canais sempre terão o mesmo tamanho de conteudo. ou seja, o mesmo tanto de blocos
+
+    for msvsChContent in msvsPairContent: # para cada conteudo de canal no par
+
+        msvsChannelBlocks = len(msvsChContent) // block_byte_size # divide o tamanho do conteudo pelo tamanho do bloco em bytes
+        msvsPairBlocks.append(msvsChannelBlocks) # guarda a divisão (o tanto de blocos) na lista
+
+    msvBlocks = max(msvsPairBlocks) # acha qual canal tem a maior quantidade de blocos para usar na construção do msv
 
     print('\nSorting channel blocks')
 
-    # Abre o arquivo de saída no modo binário para escrita
-    with open(arquivo_saida + '.msv', "wb") as saida:
-        # Itera sobre os blocos de todos os arquivos
-        for bloco in range(num_blocos):  # Para cada bloco (0, 1, 2, ...)
-            print('Block {} of {} (L and R)'.format(bloco + 1, num_blocos), end='\r') # \r "reprinta" a linha atual
-            for arquivo in arquivos_processados:  # Para cada arquivo na lista processada
+    with open(pair_name + '.msv', "wb") as msvFile: # cria o msv do par atual e abre para escrita em bytes
 
-                inicio = bloco * tamanho_multiplo  # Calcula o início do bloco
-                fim = inicio + tamanho_multiplo  # Calcula o final do bloco
+        for msvBlock in range(msvBlocks): # para cada bloco (0, 1, 2, ...)
 
-                if inicio < len(arquivo):  # Certifica-se de que o bloco existe no arquivo
-                    saida.write(arquivo[inicio:fim])  # Escreve o bloco atual no arquivo de saída
+            print('Block {} of {} (L and R)'.format(msvBlock + 1, msvBlocks), end='\r') # \r "reprinta" a linha atual
+            for msvsChContent in msvsPairContent: # para cada conteudo de canal do par
+
+                blockStart = msvBlock * block_byte_size # calcula o inicio do bloco (2 * 0x20000 = 0x40000)
+                blockEnd = blockStart + block_byte_size # o final do bloco (0x40000 + 0x20000 = 0x60000)
+
+                if blockStart < len(msvsChContent): # se o início do bloco for menor que o tamanho do conteudo do canal
+                    msvFile.write(msvsChContent[blockStart:blockEnd]) # escreve no msv o conteudo do blockstart até o blockend
     
     print('\nMSV created!')
 
@@ -277,16 +291,16 @@ if __name__ == "__main__":
         try:
             option = int(input("\nAsriel8691's GH3PS2 Thingy\n\n1 - VAGs 2 Menu music\n2 - VAG/MSV parameters\n3 - SFX Ripper\n4 - Invert images columns\n\n0 - Exit\n> "))
         except:
-            print('\nInput error\n')
+            print('\nInvalid option\n')
             continue
 
         if option == 1:
-            preparacao(option, [".vag"])
+            listfiles(option, [".vag"])
         elif option == 2 or option == 3:
-            preparacao(option, [".vag", ".wad", ".msv", ".msvs", ".isf", ".imf"])
+            listfiles(option, [".vag", ".wad", ".msv", ".msvs", ".isf", ".imf"])
         elif option == 4:
-            preparacao(option, [".png", ".jpg", ".jpeg"])
+            listfiles(option, [".png", ".jpg", ".jpeg"])
         elif option == 9:
-            print('Versão 1.4')
+            print('Versão 1.5')
     
     print('Closing...')
